0d729fa5dc4383dc486e4a1c39678f11545585c6
Melanni Tzul <melannitzul@gmail.com>
2022-03-03 14:02:59 -0600

    cambios

---
+++ src/main/java/com/mycompany/hipodromo/main/Main.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.hipodromo.main;
+
+import com.mycompany.hipodromo.Frontend.Ventana;
+
+/**
+ *
+ * @author Melanni Tzul
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Ventana NuevaVentana = new Ventana();
+        NuevaVentana.setVisible(true);
+    }
+}
